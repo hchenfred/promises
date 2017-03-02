1ed4ba77106a973f427e57501c8e77c3282424eb
hchenfred <hchen.fred@gmail.com>
2017-03-02 15:41:27 -0800

    Passed callbackReview tests.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,29 @@
 
 var fs = require('fs');
 var request = require('request');
+var https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+    }
+    var urlArray = data.split('\n');
+    callback(err, urlArray[0]);
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  https.get(url, (res) => {
+    callback(undefined, res.statusCode);
+  }).on('error', (err) => {
+    err.message = 'Invalid URI';
+    callback(err);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
