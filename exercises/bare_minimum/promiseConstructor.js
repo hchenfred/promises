89d30850b91af6cd592db1593ac74b8a81718bbb
hchenfred <hchen.fred@gmail.com>
2017-03-02 17:47:03 -0800

    Passed first test cases.

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,6 +11,16 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var urlArray = data.split('\n');
+        resolve(urlArray[0]);        
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
